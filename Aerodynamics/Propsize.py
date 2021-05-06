fe96ef0dee5c645e0e39ffc2ac531ebcd500cb4f
mavancapel <m.a.vancapel-1@student.tudelft.nl>
2021-05-06 16:50:44 +0200

    test

--- Aerodynamics/Propsize.py
+++ Aerodynamics/Propsize.py
@@ -2,22 +2,20 @@ import numpy as np
 from math import *
 import scipy.integrate as integrate
 
-
-
-
 def lift(rho,S,Cla,Cl0,alpha,rpm,R):
     Ltot = np.zeros(len(alpha))
     Lerror = np.zeros(len(alpha))
     for i in range(len(alpha)):
-        Ltot[i],Lerror[i], = integrate.quad(lambda x: 0.5*rho*S*(Cla+Cl0)*alpha[i]*(rpm*2*np.pi/60*x)**2,0,R)
+        Ltot[i],Lerror[i], = integrate.quad(lambda x: 0.5*rho*S*(Cla+Cl0)*alpha[i]*(rpm*(2*np.pi)/60*x)**2,0,R)
     return Ltot, Lerror
 
 rho = 1.225
-S = 1
+b = 0.03
 Cla = 0.0971444
 Cl0 = 0
-alpha = np.array([0,2])
-rpm = 1000
-R = 0.05
+alpha = np.array([0,2,4])
+rpm = 25000
+R = 0.15
+S = R*b
 
 print(lift(rho,S,Cla,Cl0,alpha,rpm,R))
