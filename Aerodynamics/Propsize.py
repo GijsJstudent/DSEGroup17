09bd29a856bca9a981c6316bd92cf50c4990da40
GijsJ <grjanssen@student.tudelft.nl>
2021-05-10 10:23:12 +0200

    Updated propsize file

--- Aerodynamics/Propsize.py
+++ Aerodynamics/Propsize.py
@@ -20,31 +20,53 @@ def liftrpm(rho,S,Cla,Cl0,alpha,rpm,R):
     return Ltot
 
 rho = 1.225
-b = 0.03
+AR = 5
 Cla = 0.0971444
 Cl0 = 0
-alpha1 = np.arange(0,10)
-alpha2 = 3
-rpm1 = 25000
-rpm2 = np.arange(10000,50000)
-R = 0.15
+R = np.arange(0.01,0.51,0.01)
+b = R/AR
 S = b*R
 
+maxrpm = 50000
+Ttotal = 54
+nprop = 4
+eff = 0.55
+T = Ttotal/nprop/eff
 
-L1 = liftalpha(rho,S,Cla,Cl0,alpha1,rpm1,R)
-plt.plot(alpha1,L1)
-plt.xlabel('alpha [deg]')
-plt.ylabel('Lift [N]')
-plt.title('Alpha against life for one blade of 25000 RPM')
+
+
+for alpha in range(1,20,2):
+    rpm = np.sqrt(T/(1/6*rho*S*(Cla*alpha+Cl0)*(2*np.pi/60)**2*R**3))
+    plt.plot(rpm,R,label = 'alpha=%s'%alpha)
+
+plt.vlines(maxrpm,0,0.50,colors='k',linestyles='dashed')
+plt.legend()
+plt.xlim(0,70000)
+plt.ylim(0,0.50)
+plt.title('RPM versus radius of propeller for %s propellers and a total thrust of %s N'%(nprop,Ttotal))
+plt.xlabel('Rotations per minute of the engine')
+plt.ylabel('Radius of propeller')
 plt.show()
 
 
-L2 = liftrpm(rho,S,Cla,Cl0,alpha2,rpm2,R)
-plt.plot(rpm2,L2)
-plt.xlabel('RPM')
-plt.ylabel('Lift [N]')
-plt.title('Alpha against life for one blade of 3 degrees AOA')
-plt.show()
 
+
+
+#
+# L1 = liftalpha(rho,S,Cla,Cl0,alpha1,rpm1,R)
+# plt.plot(alpha1,L1)
+# plt.xlabel('alpha [deg]')
+# plt.ylabel('Thrust [N]')
+# plt.title('Alpha against life for one blade of 25000 RPM')
+# plt.show()
+#
+#
+# L2 = liftrpm(rho,S,Cla,Cl0,alpha2,rpm2,R)
+# plt.plot(rpm2,L2)
+# plt.xlabel('RPM')
+# plt.ylabel('Thrust [N]')
+# plt.title('Alpha against life for one blade of 3 degrees AOA')
+# plt.show()
+#
 
 
